c554e2c2fc0ec0005be62777aa25b57ccf4f03ee
Nayan Kawalkar <nayankawalkar07@gmail.com>
2025-06-04 16:24:07 +0530

    bug fix

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,6 @@
-import Image from "next/image";
+
 import SearchForm from '@/components/SearchForm'
 import StartupCard, {StartupTypeCard} from '@/components/StartupCard'
-import {client } from '@/sanity/lib/client'
 import {STARTUPS_QUERY} from '@/sanity/lib/queries'
 import { sanityFetch, SanityLive} from '@/sanity/lib/live'
 
